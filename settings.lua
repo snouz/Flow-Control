6d27f80e2923b588bda5b5243a59d4e19c86c483
snouz <snooz-x@hotmail.com>
2021-12-11 15:10:48 +0100

    Update fr locale.

--- settings.lua
+++ settings.lua
@@ -1,9 +1,9 @@
 data:extend({
-	{
-		type = "bool-setting",
-		name = "flow-control-new-group",
-		setting_type = "startup",
+  {
+    type = "bool-setting",
+    name = "flow-control-new-group",
+    setting_type = "startup",
     default_value = true,
     order = "a"
-	}
+  }
 })
