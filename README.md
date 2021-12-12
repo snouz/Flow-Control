24e9bb574e6c8955e4676a13ee530e0d4cdbbe61
snouz <snooz-x@hotmail.com>
2021-12-12 18:51:06 +0100

    new entities, properties, temp icons

--- README.md
+++ README.md
@@ -1,36 +1,29 @@
 This mod adds three valves and three rotatable pipe shapes with limited connectivity.
-
 The valves and pump are unlocked by researching **Fluid handling**, the pipes have no research requirement.
 
 ## Non-return valve
-Also known as a check valve, this allows full-speed flow in one direction, indicated by the green arrow.
-
-Can output its fluid level to red/green wires.
-
-Recipe: 1 iron plate, 1 iron gear, 1 pipe
+- Also known as a check valve, this allows full-speed flow in one direction, indicated by the green arrow.
+- Can output its fluid level to red/green wires.
+- Recipe: 1 iron plate, 1 iron gear, 1 pipe
 
 ![](http://i.imgur.com/zBa2TkZ.jpg)
 
 ## Overflow valve
-Allows flow in the direction of the orange arrow, but only when the input pressure is above 80% (i.e. the attached pipe or tank is 80% full). The maximum flow rate of the overflow valve is less than a regular pipe.  Useful as an outlet for excess production, for example cracking excess heavy oil when lubricant supply exceeds demand.
-
-Can output its fluid level to red/green wires.
-
-Recipe: 1 electronic circuit, 1 iron plate, 1 iron gear, 1 pipe
+- Allows flow in the direction of the orange arrow, but only when the input pressure is above 80% (i.e. the attached pipe or tank is 80% full). The maximum flow rate of the overflow valve is less than a regular pipe.  Useful as an outlet for excess production, for example cracking excess heavy oil when lubricant supply exceeds demand.
+- Can output its fluid level to red/green wires.
+- Recipe: 1 electronic circuit, 1 iron plate, 1 iron gear, 1 pipe
 
 ![](http://i.imgur.com/CeOwHKl.jpg)
 
 ## Top-up valve
-Allows flow in the direction of the white arrow, but only when the output pressure is below 80% (i.e. the attached pipe or tank is less than 80% full). The maximum flow rate of the float valve is less than a regular pipe.  Useful for topping up or acting as a reduced-priority fluid input. Note that a small amount of fluid will remain in this valve when the pipe is emptied.
-
-Can output its fluid level to red/green wires.
-
-Recipe: 1 electronic circuit, 1 iron plate, 1 iron gear, 1 pipe
+- Allows flow in the direction of the white arrow, but only when the output pressure is below 80% (i.e. the attached pipe or tank is less than 80% full). The maximum flow rate of the float valve is less than a regular pipe.  Useful for topping up or acting as a reduced-priority fluid input. Note that a small amount of fluid will remain in this valve when the pipe is emptied.
+- Can output its fluid level to red/green wires.
+- Recipe: 1 electronic circuit, 1 iron plate, 1 iron gear, 1 pipe
 
 ![](http://i.imgur.com/Q4E5jA9.jpg)
 
 ## Pipes
-Elbow, T-junction and straight pipes, that only connect in appropriate directions. Each pipe is crafted from one normal pipe, and when mined drops a normal pipe.
+- Elbow, T-junction and straight pipes, that only connect in appropriate directions. Each pipe is crafted from one normal pipe, and when mined drops a normal pipe.
 
 ![](http://i.imgur.com/EmnmdN0.jpg)
 
@@ -38,4 +31,4 @@ Elbow, T-junction and straight pipes, that only connect in appropriate direction
 
 ## Settings
 
-* **New subgroup for pipe items** (enabled by default): Moves pipes, pumps and valves to a new line in the crafting menu
+- **New subgroup for pipe items** (enabled by default): Moves pipes, pumps and valves to a new line in the crafting menu
--- prototypes/entities.lua
+++ prototypes/entities.lua
@@ -1,26 +1,23 @@
-empty_sprite =
-{
-  filename = "__core__/graphics/empty.png",
-  priority = "extra-high",
-  width = 1,
-  height = 1,
-  frame_count = 1
-}
+empty_sprite = { filename = "__core__/graphics/empty.png", size = 1, frame_count = 1 }
+local pipe = data.raw["pipe"]["pipe"]
 
 -- Pipe Elbow ****************************************************************************
 pipe_elbow = util.table.deepcopy(data.raw["storage-tank"]["storage-tank"])
 pipe_elbow.name = "pipe-elbow"
-pipe_elbow.icon = "__Flow Control__/graphics/icon/pipe-elbow.png"
-pipe_elbow.icon_size = 32
+pipe_elbow.icon = "__Flow Control__/graphics/icons/pipe-elbow.png"
+pipe_elbow.icon_size = 64
 pipe_elbow.icon_mipmaps = nil
-pipe_elbow.minable = data.raw["pipe"]["pipe"].minable
+pipe_elbow.minable = pipe.minable
 -- pipe_elbow.corpse = "small-remnants"
 pipe_elbow.corpse = "pipe-remnants"
-pipe_elbow.max_health = data.raw["pipe"]["pipe"].max_health
-pipe_elbow.resistances = data.raw["pipe"]["pipe"].resistances
-pipe_elbow.fast_replaceable_group = data.raw["pipe"]["pipe"].fast_replaceable_group
-pipe_elbow.collision_box = data.raw["pipe"]["pipe"].collision_box
-pipe_elbow.selection_box = data.raw["pipe"]["pipe"].selection_box
+pipe_elbow.max_health = pipe.max_health
+pipe_elbow.resistances = pipe.resistances
+pipe_elbow.fast_replaceable_group = pipe.fast_replaceable_group
+pipe_elbow.collision_box = pipe.collision_box
+pipe_elbow.selection_box = pipe.selection_box
+pipe_elbow.dying_explosion = pipe.dying_explosion
+pipe_elbow.friendly_map_color = {69,130,165}
+pipe_elbow.water_reflection = nil
 pipe_elbow.fluid_box =
 {
   base_area = 1,
@@ -52,7 +49,7 @@ pipe_elbow.working_sound = nil
 -- Pipe Junction *************************************************************************
 pipe_junction = util.table.deepcopy(pipe_elbow)
 pipe_junction.name = "pipe-junction"
-pipe_junction.icon = "__Flow Control__/graphics/icon/pipe-junction.png"
+pipe_junction.icon = "__Flow Control__/graphics/icons/pipe-junction.png"
 pipe_junction.fluid_box.pipe_connections =
 {
   { position = {1, 0} },
@@ -79,7 +76,7 @@ pipe_junction.pictures =
 -- Pipe Straight *************************************************************************
 pipe_straight = util.table.deepcopy(pipe_elbow)
 pipe_straight.name = "pipe-straight"
-pipe_straight.icon = "__Flow Control__/graphics/icon/pipe-straight.png"
+pipe_straight.icon = "__Flow Control__/graphics/icons/pipe-straight.png"
 pipe_straight.fluid_box.pipe_connections =
 {
   { position = {0, -1} },
@@ -100,11 +97,12 @@ pipe_straight.pictures =
   flow_sprite = empty_sprite
 }
 
+-------------------------------------------------- VALVES ---------------------------------------------------------------------
+
 -- Check Valve ***************************************************************************
 check_valve = util.table.deepcopy(pipe_straight)
 check_valve.name = "check-valve"
-check_valve.icon = "__Flow Control__/graphics/icon/check-valve.png"
--- check_valve.icon_size = 32
+check_valve.icon = "__Flow Control__/graphics/icons/check-valve.png"
 check_valve.minable = {mining_time = 1, result = "check-valve"}
 check_valve.fluid_box =
 {
@@ -121,93 +119,40 @@ check_valve.pictures.picture =
   sheet =
   {
     filename = "__Flow Control__/graphics/entity/check-valve/check-valve.png",
-    priority = "extra-high",
     frames = 4,
-    width = 58,
-    height = 55,
-    shift = {0.28125, -0.078125}
-  }
-}
-check_valve.circuit_wire_connection_points =
-{
-  {
-    shadow =
-    {
-      red = {0.171875, 0.140625},
-      green = {0.171875, 0.265625},
-    },
-    wire =
-    {
-      red = {-0.53125, -0.15625},
-      green = {-0.53125, 0},
-    }
-  },
-  {
-    shadow =
-    {
-      red = {0.890625, 0.703125},
-      green = {0.75, 0.75},
-    },
-    wire =
-    {
-      red = {0.34375, 0.28125},
-      green = {0.34375, 0.4375},
-    }
-  },
-  {
-    shadow =
-    {
-      red = {0.15625, 0.0625},
-      green = {0.09375, 0.125},
-    },
-    wire =
-    {
-      red = {-0.53125, -0.09375},
-      green = {-0.53125, 0.03125},
-    }
-  },
-  {
-    shadow =
-    {
-      red = {0.796875, 0.703125},
-      green = {0.625, 0.75},
-    },
-    wire =
-    {
-      red = {0.40625, 0.28125},
-      green = {0.40625, 0.4375},
+    width = 64,
+    height = 64,
+    hr_version = {
+      filename = "__Flow Control__/graphics/entity/check-valve/hr-check-valve.png",
+      frames = 4,
+      width = 128,
+      height = 128,
+      scale = 0.5,
     }
   }
 }
+check_valve.circuit_wire_connection_points = circuit_connector_definitions["inserter"].points
 check_valve.circuit_connector_sprites = circuit_connector_definitions["inserter"].sprites
--- {
-  -- get_circuit_connector_sprites({-0.40625, -0.3125}, nil, 24),
-  -- get_circuit_connector_sprites({0.125, 0.21875}, {0.34375, 0.40625}, 18),
-  -- get_circuit_connector_sprites({-0.40625, -0.25}, nil, 24),
-  -- get_circuit_connector_sprites({0.203125, 0.203125}, {0.25, 0.40625}, 18),
--- }
-check_valve.circuit_wire_max_distance =
-  data.raw["storage-tank"]["storage-tank"].circuit_wire_max_distance
+check_valve.circuit_wire_max_distance = data.raw["storage-tank"]["storage-tank"].circuit_wire_max_distance
 
 -- Overflow Valve ************************************************************************
 overflow_valve = util.table.deepcopy(check_valve)
 overflow_valve.name = "overflow-valve"
-overflow_valve.icon = "__Flow Control__/graphics/icon/overflow-valve.png"
+overflow_valve.icon = "__Flow Control__/graphics/icons/overflow-valve.png"
 overflow_valve.minable.result = "overflow-valve"
 overflow_valve.fluid_box.base_level = 0.8
-overflow_valve.pictures.picture.sheet.filename =
-  "__Flow Control__/graphics/entity/overflow-valve/overflow-valve.png"
+overflow_valve.pictures.picture.sheet.filename = "__Flow Control__/graphics/entity/overflow-valve/overflow-valve.png"
+overflow_valve.pictures.picture.sheet.hr_version.filename = "__Flow Control__/graphics/entity/overflow-valve/hr-overflow-valve.png"
 
 -- Underflow Valve ***********************************************************************
 underflow_valve = util.table.deepcopy(overflow_valve)
 underflow_valve.name = "underflow-valve"
-underflow_valve.icon = "__Flow Control__/graphics/icon/underflow-valve.png"
+underflow_valve.icon = "__Flow Control__/graphics/icons/underflow-valve.png"
 underflow_valve.minable.result = "underflow-valve"
 underflow_valve.fluid_box.base_level = -0.2
-underflow_valve.pictures.picture.sheet.filename =
-  "__Flow Control__/graphics/entity/underflow-valve/underflow-valve.png"
+underflow_valve.pictures.picture.sheet.filename = "__Flow Control__/graphics/entity/underflow-valve/underflow-valve.png"
+underflow_valve.pictures.picture.sheet.hr_version.filename = "__Flow Control__/graphics/entity/underflow-valve/hr-underflow-valve.png"
 
--- Insert Entities ***********************************************************************
 data:extend(
 {
   check_valve,
