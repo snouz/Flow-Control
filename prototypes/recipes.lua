1dcf53e94ed9dc99e14752d4498a1212655d7d85
Hein Wessels <heinwessels93@gmail.com>
2024-11-08 22:13:10 +0100

    Loads in 2.0!

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -2,47 +2,6 @@ data:extend(
 {
   {
     type = "recipe",
-    name = "check-valve",
-    energy_required = 2,
-    enabled = false,
-    ingredients =
-    {
-      {type = "item", name = "iron-plate", amount = 1},
-      {type = "item", name = "iron-gear-wheel", amount = 1},
-      {type = "item", name = "pipe",amount =  1}
-    },
-    results = {{type = "item", name = "check-valve", amount = 1}},
-  },
-  {
-    type = "recipe",
-    name = "overflow-valve",
-    energy_required = 2,
-    enabled = false,
-    ingredients =
-    {
-      {type = "item", name = "electronic-circuit", amount = 1},
-      {type = "item", name = "iron-plate", amount = 1},
-      {type = "item", name = "iron-gear-wheel", amount = 1},
-      {type = "item", name = "pipe", amount = 1}
-    },
-    results = {{type = "item", name = "overflow-valve", amount = 1}},
-  },
-  {
-    type = "recipe",
-    name = "underflow-valve",
-    energy_required = 2,
-    enabled = false,
-    ingredients =
-    {
-      {type = "item", name = "electronic-circuit", amount = 1},
-      {type = "item", name = "iron-plate", amount = 1},
-      {type = "item", name = "iron-gear-wheel", amount = 1},
-      {type = "item", name = "pipe", amount = 1}
-    },
-    results = {{type = "item", name = "underflow-valve", amount = 1}},
-  },
-  {
-    type = "recipe",
     name = "pipe-elbow",
     energy_required = 0.01,
     ingredients =
