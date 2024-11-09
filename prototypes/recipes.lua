59cc7b0b6a5a6a53d445be3f97a224efd31de7f5
Hein Wessels <heinwessels93@gmail.com>
2024-11-09 21:56:32 +0100

    Fixed pipe section recipes being available from game start, and not after technology research.

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -1,3 +1,33 @@
+---@type data.TechnologyPrototype?
+local tech_to_unlock
+for _, technology in pairs(data.raw.technology) do
+    if technology.effects then			
+        for index, effect in pairs(technology.effects) do
+            if effect.type == "unlock-recipe" then
+                if effect.recipe == "pipe" then
+                  tech_to_unlock = technology
+
+                  table.insert(tech_to_unlock.effects, index, {
+                    type = "unlock-recipe",
+                    recipe = "pipe-elbow"
+                  })
+                  table.insert(tech_to_unlock.effects, index, {
+                    type = "unlock-recipe",
+                    recipe = "pipe-junction"
+                  })
+                  table.insert(tech_to_unlock.effects, index, {
+                    type = "unlock-recipe",
+                    recipe = "pipe-straight"
+                  })
+
+                  break
+                end
+            end
+        end
+        if tech_to_unlock then break end
+    end
+end
+
 data:extend(
 {
   {
@@ -9,6 +39,7 @@ data:extend(
       {type = "item", name = "pipe", amount = 1}
     },
     results = {{type = "item", name = "pipe-elbow", amount = 1}},
+    enabled = tech_to_unlock == nil,
   },
   {
     type = "recipe",
@@ -19,6 +50,7 @@ data:extend(
       {type = "item", name = "pipe", amount = 1}
     },
     results = {{type = "item", name = "pipe-junction", amount = 1}},
+    enabled = tech_to_unlock == nil,
   },
   {
     type = "recipe",
@@ -29,5 +61,6 @@ data:extend(
       {type = "item", name = "pipe", amount = 1}
     },
     results = {{type = "item", name = "pipe-straight", amount = 1}},
+    enabled = tech_to_unlock == nil,
   }
 })
